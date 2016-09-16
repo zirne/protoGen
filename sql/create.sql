0aa7126bd50666daa973f41e0bbbc604f912f960
root <root@mlg-laptop.bt.cx>
2016-09-16 16:35:53 +0200

    Fix bug with mysql timestamp syntax

--- sql/create.sql
+++ sql/create.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `tbl_meeting` (
   `enabled` tinyint(1) NOT NULL,
   `editable` tinyint(1) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `edited` timestamp NOT NULL,
+  `edited` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
 
