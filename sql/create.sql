30259491b4edaee4e468713510f6440ebe7f48b6
Erik Einarsson <enriz@enriz.se>
2016-10-03 15:23:21 +0200

    Added SQL query for storing file attachments

--- sql/create.sql
+++ sql/create.sql
@@ -9,3 +9,13 @@ CREATE TABLE IF NOT EXISTS `tbl_meeting` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
 
+CREATE TABLE IF NOT EXISTS `protogen`.`tbl_attachments` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` TEXT COLLATE utf8_swedish_ci NOT NULL,
+  `type` TEXT COLLATE utf8_swedish_ci NOT NULL,
+  `data` BLOB NOT NULL,
+  `meetingID` INT(11) NOT NULL,
+  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `edited` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
