1a795322d079be0bfd3ea3e03eca8fe5b9eade28
johanmlg <johanmlg@mlg-laptop.bt.cx>
2016-10-09 22:47:46 +0200

    Support for mysql 5.5

--- sql/create.sql
+++ sql/create.sql
@@ -4,20 +4,20 @@ CREATE TABLE IF NOT EXISTS `tbl_meeting` (
   `title` text COLLATE utf8_swedish_ci NOT NULL,
   `enabled` tinyint(1) NOT NULL,
   `editable` tinyint(1) NOT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `edited` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created` timestamp DEFAULT 0 NOT NULL ,
+  `edited` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
 
-CREATE TABLE IF NOT EXISTS `protogen`.`tbl_attachments` (
+CREATE TABLE IF NOT EXISTS `tbl_attachments` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `name` TEXT COLLATE utf8_swedish_ci NOT NULL,
   `type` TEXT COLLATE utf8_swedish_ci NOT NULL,
   `data` LONGBLOB NOT NULL,
   `meetingID` INT(11) NOT NULL,
   `size` TEXT COLLATE utf8_swedish_ci NOT NULL,
-  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `edited` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted` tinyint(1) NOT NULL,
+  `created` TIMESTAMP DEFAULT 0 NOT NULL,
+  `edited` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
